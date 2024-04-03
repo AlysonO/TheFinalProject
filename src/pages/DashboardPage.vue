382f048cdfca67a52a0c7aecc0373d1fdbd98c4f
Alyson Okinda <alysonokinda@gmail.com>
2024-04-03 13:15:13 +0200

    basic pages and routing

---
+++ src/pages/DashboardPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-page class="flex flex-center">
+    <h1>Dashboard</h1>
+    <p>Coming Soon</p>
+    <p>Here will be a dashboard with all the data you need</p>
+    <p>Help</p>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'DashboardPage'
+})
+</script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -4,8 +4,15 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
+      { path: '', component: () => import('pages/IndexPage.vue') },
+      { path: 'dashboard', component: () => import('pages/DashboardPage.vue') },
+      { path: 'manages', component: () => import('pages/ManagesPage.vue') },
+      { path: 'entretiens', component: () => import('pages/EntretienPage.vue') }
     ]
+  },
+  {
+    path: '/login',
+    component: () => import('layouts/LoginLayout.vue')
   },
 
   // Always leave this as last one,
