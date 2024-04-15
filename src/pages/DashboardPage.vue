3d1e89be0976a31c62a7c8a8b509004646b5b444
Alyson Okinda <alysonokinda@gmail.com>
2024-04-15 17:23:05 +0200

    dashboard page

--- src/pages/DashboardPage.vue
+++ src/pages/DashboardPage.vue
@@ -1,13 +1,45 @@
 <template>
-  <q-page class="flex flex-center">
-    <h1>Dashboard</h1>
-    <p>Coming Soon</p>
-    <p>Here will be a dashboard with all the data you need</p>
-    <p>Help</p>
+  <q-page class="q-pa-md">
+    <div class="text-h2 q-pa-lg">Dashboard</div>
+    <div class="q-pa-md row items-start q-gutter-lg">
+      <q-card class="col-3">
+        <q-card-section>
+          <div class="text-h5">Managés</div>
+          <ul>
+            <li v-for="person in manages" :key="person">{{ person }}</li>
+          </ul>
+        </q-card-section>
+      </q-card>
+
+      <q-card class="col-3">
+        <q-card-section>
+          <div class="text-h5">Mon manager</div>
+        </q-card-section>
+      </q-card>
+
+      <q-card class="col-3">
+        <q-card-section>
+          <div class="text-h5">Prochains entretiens</div>
+          <div v-for="date in entretien" :key="date">
+            <div>Managé: {{ date.managee }}</div>
+            <div>Date: {{ date.date }}</div>
+          </div>
+        </q-card-section>
+      </q-card>
+
+      <q-card class="col-3">
+        <q-card-section>
+          <div class="text-h5">Mon prochain entretien personnel</div>
+          <div>Manager:</div>
+          <div>Date:</div>
+        </q-card-section>
+      </q-card>
+    </div>
   </q-page>
 </template>
 
 <script>
+import { ref } from 'vue'
 import { defineComponent } from 'vue'
 
 export default defineComponent({
