43bec49c77e8570aed6522f88031438feb83de6a
Alyson Okinda <alysonokinda@gmail.com>
2024-04-16 09:04:44 +0200

    entretiens set up

--- src/pages/EntretienPage.vue
+++ src/pages/EntretienPage.vue
@@ -1,16 +1,50 @@
 <template>
-  <q-page class="flex flex-center">
-    <h1>Dashboard</h1>
-    <p>Coming Soon</p>
-    <p>Here will be a dashboard with all the data you need</p>
-    <p>Help</p>
+  <q-page class="q-pa-md">
+    <q-carousel
+      v-model="slide"
+      infinite
+      swipeable
+      control-color="black"
+      navigation
+      padding
+      arrows
+      class="outline shadow-1 rounded-borders"
+    >
+      <q-carousel-slide name="style" class="column no-wrap flex-center">
+        <div class="q-mt-md text-center">
+          <div class="text-h3 q-pa-lg">Tous les entretiens</div>
+          <div>
+            <q-table> </q-table>
+          </div>
+        </div>
+      </q-carousel-slide>
+
+      <q-carousel-slide name="tv" class="column no-wrap flex-center">
+        <div class="q-mt-md text-center">
+          <div class="text-h3">Mes entretiens</div>
+        </div>
+      </q-carousel-slide>
+
+      <q-carousel-slide name="layers" class="column no-wrap flex-center">
+        <div class="q-mt-md text-center">
+          <div class="text-h3">Mes entretiens</div>
+        </div>
+      </q-carousel-slide>
+    </q-carousel>
   </q-page>
 </template>
 
 <script>
 import { defineComponent } from 'vue'
+import { ref } from 'vue'
 
 export default defineComponent({
-  name: 'EntretienPage'
+  name: 'EntretienPage',
+
+  setup() {
+    return {
+      slide: ref('style')
+    }
+  }
 })
 </script>
